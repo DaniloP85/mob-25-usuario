abcfd765bd8174ecc8a969f42298453ccc57c2aa
Danilo Santos <danilopsnts@gmail.com>
2022-06-26 16:45:38 -0300

    add exemplo da aula

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cfg = require("./src/config/cfg");
+const notfound = require("./src/middleware/notfound");
+const routecliente = require("./src/routes/cliente");
+const routeusuario = require("./src/routes/usuario");
+const cors = require("cors");
+
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("combined"));
+app.use(cors());
+
+mongoose.connect(cfg.db_path, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use("/clientes",routecliente);
+app.use("/usuarios",routeusuario);
+app.use(notfound);
+
+
+app.listen(3000, () =>
+  console.log(`Servidor on-line. em http://localhost:3000`)
+);
---
+++ src/model/cliente.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const schema_cliente = new mongoose.Schema({
+  nomecompleto: { type: String },
+  apikey: { type: String },
+  email: { type: String, unique: true },
+  telefone: { type: String },
+  endereco: { type: String },
+});
+module.exports = mongoose.model("Cliente", schema_cliente);
