f1a5c3c1bd8e9806ce8b337c8b11868cf1beabba
Danilo Santos <danilopsnts@gmail.com>
2022-07-03 12:49:41 -0300

    add banco local

--- src/config/cfg.js
+++ src/config/cfg.js
@@ -3,8 +3,7 @@ const cfg = () => {
     jwt_secret: "#V$Code%",
     jwt_expires: "2d",
     salt: 10,
-    db_path:
-      "mongodb+srv://edilson:Alunos123@clusterapi.fkxyi.mongodb.net/bancomicro?retryWrites=true&w=majority",
+    db_path: "mongodb://localhost:27017/local",
   };
 };
 module.exports = cfg();
