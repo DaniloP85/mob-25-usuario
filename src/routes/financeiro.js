52e2354a62ae711ae79d12ac5b02b9f1e9e8d8bf
Danilo Santos <danilopsnts@gmail.com>
2022-07-03 19:03:30 -0300

    add update produtos financeiros

--- src/routes/financeiro.js
+++ src/routes/financeiro.js
@@ -26,4 +26,33 @@ route.post("/cadastro", verificar_token, (req, res) => {
   });
 });
 
+route.put("/atualizar/:id", verificar_token, (req, res) => {
+  Cliente.findOne({ _id: req.body.id_cliente }, (erro, dados) => {
+    if (erro) {
+      return res
+        .status(500)
+        .send({ output: `Erro ao tentar cadastrar -> ${erro}` });
+    }
+
+    if (dados === null) {
+      return res.status(204).send({ output: `Cliente nao localizado` });
+    }
+
+    const InfoFinanceiras = {
+      _id: req.params.id,
+      nome_banco: req.body.nome_banco,
+      tipo_conta: req.body.tipo_conta,
+      nome_titular: req.body.nome_titular,
+      limite_cartao: req.body.limite_cartao,
+      id_cliente: req.body.id_cliente,
+    };
+
+    ProdutosFinanceiros.update(InfoFinanceiras, (err, data) => {
+      if (!err) {
+        return res.status(202).send({ output: "ok", payload: data });
+      }
+    });
+  });
+});
+
 module.exports = route;
